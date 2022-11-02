9136c690ea83f533422e3bda150a8dbea90b63c8
Ashish Shende <shende.ashish03@gmail.com>
2022-11-02 23:20:52 +0530

    New Practice

---
+++ day1/src/Example.java
@@ -0,0 +1,16 @@
+/*
+  To Do : Go through individual classes, look for compiler errs , if none 
+   what will be o/p ? 
+   Run the code & confirm the o/p
+*/
+
+/*
+   This is a simple Java program.
+   Call this file "Example.java".
+*/
+class Example {
+  // Your program begins with a call to main().
+  public static void main(String args[]) {
+    System.out.println("This is a simple Java program.");
+  }
+}
---
+++ day1/src/ForTest.java
@@ -0,0 +1,13 @@
+/*
+  Demonstrate the for loop.
+  
+  Call this file "ForTest.java".
+*/
+class ForTest {
+  public static void main(String args[]) {
+   
+  
+    for(int x = 0; x<10; x =x+2)
+      System.out.println("This is x: " + x);
+  }
+}
